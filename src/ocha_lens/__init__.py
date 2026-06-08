3f2c2c269153ece206e5e56a4bda3f260b6ea474
Zack <zackarno@gmail.com>
2026-06-08 12:36:52 -0600

    Export adam datasource at the package top level
    adam was reachable only as ocha_lens.datasources.adam; export it from
    the package root alongside gdacs/nhc/ibtracs/ecmwf_storm for parity.

--- src/ocha_lens/__init__.py
+++ src/ocha_lens/__init__.py
@@ -1,4 +1,4 @@
 from ._version import version as __version__  # noqa: F401
-from .datasources import ecmwf_storm, gdacs, ibtracs, nhc
+from .datasources import adam, ecmwf_storm, gdacs, ibtracs, nhc
 
-__all__ = ["ibtracs", "ecmwf_storm", "gdacs", "nhc"]
+__all__ = ["ibtracs", "ecmwf_storm", "gdacs", "nhc", "adam"]
